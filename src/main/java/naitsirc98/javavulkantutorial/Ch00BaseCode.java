46ce67351759b0a683c9e860ad3b5b21282a0b91
Cristian Herrera <naitsirc98@gmail.com>
2020-02-03 21:55:23 +0000

    '01_instance_creation' complete

---
+++ src/main/java/naitsirc98/javavulkantutorial/Ch00BaseCode.java
@@ -0,0 +1,72 @@
+package naitsirc98.javavulkantutorial;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Ch00BaseCode {
+
+    private static class HelloTriangleApplication {
+
+        private static final int WIDTH = 800;
+        private static final int HEIGHT = 600;
+
+        // ======= FIELDS ======= //
+
+        private long window;
+
+        // ======= METHODS ======= //
+
+        public void run() {
+            initWindow();
+            initVulkan();
+            mainLoop();
+            cleanup();
+        }
+
+        private void initWindow() {
+
+            if(!glfwInit()) {
+                throw new RuntimeException("Cannot initialize GLFW");
+            }
+
+
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+            String title = getClass().getEnclosingClass().getSimpleName();
+
+            window = glfwCreateWindow(WIDTH, HEIGHT, title, NULL, NULL);
+
+            if(window == NULL) {
+                throw new RuntimeException("Cannot create window");
+            }
+        }
+
+        private void initVulkan() {
+
+        }
+
+        private void mainLoop() {
+
+            while(!glfwWindowShouldClose(window)) {
+                glfwPollEvents();
+            }
+
+        }
+
+        private void cleanup() {
+
+            glfwDestroyWindow(window);
+
+            glfwTerminate();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        HelloTriangleApplication app = new HelloTriangleApplication();
+
+        app.run();
+    }
+}
---
+++ src/main/java/naitsirc98/javavulkantutorial/Ch01InstanceCreation.java
@@ -0,0 +1,117 @@
+package naitsirc98.javavulkantutorial;
+
+import org.lwjgl.PointerBuffer;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.vulkan.VkApplicationInfo;
+import org.lwjgl.vulkan.VkInstance;
+import org.lwjgl.vulkan.VkInstanceCreateInfo;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFWVulkan.glfwGetRequiredInstanceExtensions;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+import static org.lwjgl.vulkan.VK10.*;
+
+public class Ch01InstanceCreation {
+
+    private static class HelloTriangleApplication {
+
+        private static final int WIDTH = 800;
+        private static final int HEIGHT = 600;
+
+        // ======= FIELDS ======= //
+
+        private long window;
+        private VkInstance instance;
+
+        // ======= METHODS ======= //
+
+        public void run() {
+            initWindow();
+            initVulkan();
+            mainLoop();
+            cleanup();
+        }
+
+        private void initWindow() {
+
+            if(!glfwInit()) {
+                throw new RuntimeException("Cannot initialize GLFW");
+            }
+
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+            String title = getClass().getEnclosingClass().getSimpleName();
+
+            window = glfwCreateWindow(WIDTH, HEIGHT, title, NULL, NULL);
+
+            if(window == NULL) {
+                throw new RuntimeException("Cannot create window");
+            }
+        }
+
+        private void initVulkan() {
+            createInstance();
+        }
+
+        private void mainLoop() {
+
+            while(!glfwWindowShouldClose(window)) {
+                glfwPollEvents();
+            }
+
+        }
+
+        private void cleanup() {
+
+            glfwDestroyWindow(window);
+
+            glfwTerminate();
+        }
+
+        private void createInstance() {
+
+            try(MemoryStack stack = stackPush()) {
+
+                // Use calloc to initialize the structs with 0s. Otherwise, the program can crash due to random values
+
+                VkApplicationInfo appInfo = VkApplicationInfo.callocStack(stack);
+
+                appInfo.sType(VK_STRUCTURE_TYPE_APPLICATION_INFO);
+                appInfo.pApplicationName(stack.UTF8Safe("Hello Triangle"));
+                appInfo.applicationVersion(VK_MAKE_VERSION(1, 0, 0));
+                appInfo.pEngineName(stack.UTF8Safe("No Engine"));
+                appInfo.engineVersion(VK_MAKE_VERSION(1, 0, 0));
+                appInfo.apiVersion(VK_API_VERSION_1_0);
+
+                VkInstanceCreateInfo createInfo = VkInstanceCreateInfo.callocStack(stack);
+
+                createInfo.sType(VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO);
+                createInfo.pApplicationInfo(appInfo);
+                // enabledExtensionCount is implicitly set when you call ppEnabledExtensionNames
+                createInfo.ppEnabledExtensionNames(glfwGetRequiredInstanceExtensions());
+                // same with enabledLayerCount
+                createInfo.ppEnabledLayerNames(null);
+
+                // We need to retrieve the pointer of the created instance
+                PointerBuffer instancePtr = stack.mallocPointer(1);
+
+                if(vkCreateInstance(createInfo, null, instancePtr) != VK_SUCCESS) {
+                    throw new RuntimeException("Failed to create instance");
+                }
+
+                instance = new VkInstance(instancePtr.get(0), createInfo);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        HelloTriangleApplication app = new HelloTriangleApplication();
+
+        app.run();
+    }
+}
